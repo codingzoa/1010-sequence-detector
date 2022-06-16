e778c6f35aeb0be6a9fb9538c634bc5d95bbf376
codingzoa <105957917+codingzoa@users.noreply.github.com>
2022-06-17 00:02:15 +0900

    Add files via upload

---
+++ with Mealy machine.vhd
@@ -0,0 +1,52 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+entity Seq_Detector_Mealy is
+  port(
+  CLK : in std_logic;           --input : CLK, RESET(asyn), X
+  RESET : in std_logic;
+  X: in std_logic;
+  Z: out std_logic              --output : Z(when sequence is '1010' Z='1')
+  );
+end Seq_Detector_Mealy;
+
+architecture Behavioral of Seq_Detector_Mealy is
+    type states is (s0,s1,s2,s3);               --최소 state 개수가 4개
+    signal state, next_state : states;             -- state와 next state는 s0, s1, s2, s3일 수 있다.
+    
+begin
+--state transition
+    process(RESET, CLK)                             
+        begin                                       --state reg는 sequential logic이기 때문에 clk에 따라 값 변화
+            if (RESET = '0') then                   --RESET은 asynchronous, active low이기 때문에 
+            state<=s0;                              --CLK와 별개로 0이면 initial 상태로 초기화
+            elsif (CLK='1' and CLK'event) then      --RESET이 1일 때 CLK 상승 엣지에서 
+            state<= next_state;                     --next state로 변화
+            end if;
+    end process;
+    
+    --outputs, next state
+    process(X, state)                           --mealy machine의 출력은 입력과 현재 state에 따라 바뀜
+    begin
+        case state is                           
+         when s0 =>
+            if X='0' then next_state<=s0; Z<='0';    --s0일 때 X가 0이면 next state는 s0, 출력 0
+            else next_state<= s1;Z<='0';             --s0일 때 X가 1이면 next state는 s1, 출력 0
+            end if;
+         when s1 =>
+            if X='0' then next_state<=s2;Z<='0';   --s1일 때 X가 0이면 next state는 s2, 출력 0
+            else next_state<= s1;Z<='0';           --s1일 때 X가 1이면 next state는 s1, 출력 0
+            end if;
+         when s2 =>
+            if X='0' then next_state<=s0;Z<='0';   --s2일 때 X가 0이면 next state는 s0, 출력 0
+            else next_state<= s3;Z<='0';           --s2일 때 X가 1이면 next state는 s3, 출력 0
+            end if;
+         when s3 =>
+            if X='0' then next_state<=s2;Z<='1';   --s3일 때 X가 0이면 next state는 s2, 출력 1
+            else next_state<= s1; Z<='0';          --s3일 때 X가 1이면 next state는 s1, 출력 0
+            end if;
+         when others => null;
+        end case;
+    end process;
+
+end Behavioral;
---
+++ with Moore machine.vhd
@@ -0,0 +1,76 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Seq_Detector_Moore_tb is
+end Seq_Detector_Moore_tb;
+
+architecture Behavioral of Seq_Detector_Moore_tb is
+    component Seq_Detector_Moore is         --설계한 시퀀스 디텍터를 불러와서 스펙에 맞게 동작하는지 확인
+    port(                                   --시퀀스 디텍터의 입력과 출력 그대로
+    CLK: in std_logic;
+    RESET: in std_logic;
+    X: in std_logic;
+    Z: out std_logic);
+    end component;
+    
+    signal CLK :std_logic;       
+    signal RESET: std_logic;
+    signal X,Z: std_logic;
+    
+begin
+    --테스트벤치와 설계한 시퀀스 디텍터의 입력과 출력 연결
+    SD: Seq_Detector_Moore port map(CLK=>CLK, RESET=>RESET, X=>X, Z=>Z);
+    
+    rst_operation :process  --reset이 0일 때 초기 상태로 돌아가는지 확인하기 위함
+    begin
+    RESET <= '0';
+    wait for 15ns;
+    RESET <= '1';
+    wait for 80ns;
+    RESET <= '0';
+    wait for 10ns;
+    RESET<= '1';
+    wait;
+    end process;
+    
+    clk_operation : process    --클락을 인가하기 위함
+    begin
+    CLK<='1';
+    wait for 5ns;
+     CLK<='0';
+    wait for 5ns;
+    end process;
+    
+    input_operation : process       --출력이 잘 나오는지 확인하기 위한 입력 인가
+    begin
+    X<='1';
+    wait for 25ns;
+    X<='0';
+    wait for 10ns;
+    X<='1';
+    wait for 10ns;
+    X<='0';
+    wait for 10ns;
+    X<='1';
+    wait for 10ns;
+    X<='0';
+    wait for 10ns;
+    X<='1';
+    wait for 10ns;
+    X<='0';
+    wait for 10ns;
+    X<='1';
+    wait for 10ns;
+    X<='0';
+    wait for 10ns;
+    X<='1';
+    wait for 10ns;
+    X<='0';
+    wait for 10ns;
+    X<='1';
+    wait for 10ns;
+    X<='0';
+    wait;
+    end process;
+end Behavioral;
